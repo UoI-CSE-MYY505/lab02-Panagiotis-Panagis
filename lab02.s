cbf5e46ee0492705664d02f03ee7f9a7b493eae9
Panagiotis-Panagis <cs05317@uoi.gr>
2024-10-20 21:38:58 +0300

    lab02_ready_5317

--- lab02.s
+++ lab02.s
@@ -11,6 +11,19 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+search_last_match:
+    add  s0, zero, zero # if not found match  
+loop:
+    beq a1,zero, done
+    lw t1, 0(a0)
+    bne t1, a2, next 
+    add s0, a0, zero
+    
+next: # go to next word
+    addi a0, a0 , 4
+    addi a1, a1, -1
+    j loop
+    
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
